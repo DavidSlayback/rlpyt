122ef9754c31325b16b3045dddac5e49f19d6f92
7thStringofZhef <david.slayback2010@gmail.com>
2021-01-22 14:28:38 -0500

    Added modified gym_pomdps repo for easy installation

---
+++ gym-pomdps/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+from gym_pomdps import __version__
+
+setup(
+    name='gym-pomdps',
+    version=__version__,
+    description='Gym flat POMDP environments',
+    author='Andrea Baisero',
+    author_email='andrea.baisero@gmail.com',
+    url='https://github.com/abaisero/gym-pomdps',
+    packages=['gym_pomdps'],
+    package_data={'': ['*.pomdp']},
+    install_requires=['gym', 'numpy', 'rl-parsers',],
+    license='MIT',
+)
