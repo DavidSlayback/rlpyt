ffc865e10a2743aef0a3578844cdaf1582fb5529
7thStringofZhef <david.slayback2010@gmail.com>
2021-01-28 11:51:16 -0500

    Updated to test far fewer environments, added extra field to config after the fact to make it easier to plot later

--- rlpyt/experiments/scripts/pomdp/pg/launch/launch_pomdp_gpu.py
+++ rlpyt/experiments/scripts/pomdp/pg/launch/launch_pomdp_gpu.py
@@ -25,12 +25,14 @@ default_key_rnn = 'hallway_5e5_rnn'
 oc_key_rnn = 'hallway_5e5_rnn_oc'
 # Param options
 RNN = list(zip(['lstm', 'gru']))
-RNN = list(zip(['lstm']))
+# RNN = list(zip(['lstm']))
 RNN_SIZE = list(zip([64, 128, 256]))
 FOMDP = list(zip([False, True]))
 INTEREST = list(zip([False, True]))
 NUM_OPTIONS = list(zip([2, 4]))
-ENVS = list(zip(list(OPTIMAL_RETURNS.keys())))  # All environments with comparable optimal returns
+OC_DELIB = list(zip([0., 0.05, 0.5]))
+# ENVS = list(zip(list(OPTIMAL_RETURNS.keys())))  # All environments with comparable optimal returns
+ENVS = list(zip(['POMDP-hallway-continuing-v0', 'POMDP-hallway2-continuing-v0', 'POMDP-rock_sample_7_8-continuing-v0']))  # Subselect
 # Variant keys
 lr_key = [("algo", "learning_rate")]
 delib_key = [("algo", "delib_cost")]
@@ -45,67 +47,70 @@ fomdp_key = [("env", "fomdp")]
 env_names = ["{}".format(*v) for v in ENVS]
 rnn_names = ["{}".format(*v) for v in RNN]
 rnn_size_names = ["RNNH_{}".format(*v) for v in RNN_SIZE]
+delib_names = ["DELIB_{}".format(*v) for v in OC_DELIB]
 obs_names = ["OMNI_{}".format(*v) for v in FOMDP]
 int_names = ["INT_{}".format(*v) for v in INTEREST]
 nopt_names = ["NOPT_{}".format(*v) for v in NUM_OPTIONS]
 
 # A2C
-# experiment_title = "A2C_Pomdp"
-# variant_levels = list()
-# variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
-# variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
-# variants, log_dirs = make_variants(*variant_levels)
-# run_experiments(
-#     script=path_a2c,
-#     affinity_code=affinity_code,
-#     experiment_title=experiment_title,
-#     runs_per_setting=runs_per_setting,
-#     variants=variants,
-#     log_dirs=log_dirs,
-#     common_args=(default_key,),
-# )
-
+experiment_title = "A2C_Pomdp"
+variant_levels = list()
+variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
+variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
+variants, log_dirs = make_variants(*variant_levels)
+run_experiments(
+    script=path_a2c,
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+    common_args=(default_key,),
+)
+#
 # A2C RNN
-# experiment_title = "A2CRnn_Pomdp"
-# variant_levels = list()
-# variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
-# variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
-# variant_levels.append(VariantLevel(rnn_type_key, RNN, rnn_names))  # Types of recurrency
-# variant_levels.append(VariantLevel(rnn_type_key, RNN_SIZE, rnn_size_names))  # Sizes of recurrency
-# variants, log_dirs = make_variants(*variant_levels)
-# run_experiments(
-#     script=path_a2c_rnn,
-#     affinity_code=affinity_code,
-#     experiment_title=experiment_title,
-#     runs_per_setting=runs_per_setting,
-#     variants=variants,
-#     log_dirs=log_dirs,
-#     common_args=(default_key_rnn,),
-# )
-
+experiment_title = "A2CRnn_Pomdp"
+variant_levels = list()
+variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
+variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
+variant_levels.append(VariantLevel(rnn_type_key, RNN, rnn_names))  # Types of recurrency
+variant_levels.append(VariantLevel(rnn_type_key, RNN_SIZE, rnn_size_names))  # Sizes of recurrency
+variants, log_dirs = make_variants(*variant_levels)
+run_experiments(
+    script=path_a2c_rnn,
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+    common_args=(default_key_rnn,),
+)
+#
 # A2OC
-# experiment_title = "A2OC_Pomdp"
-# variant_levels = list()
-# variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
-# variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
-# variant_levels.append(VariantLevel(nopt_key, NUM_OPTIONS, nopt_names))  # Number of options
-# variant_levels.append(VariantLevel(interest_key, INTEREST, int_names))  # Use of interest function
-# variants, log_dirs = make_variants(*variant_levels)
-# run_experiments(
-#     script=path_a2oc,
-#     affinity_code=affinity_code,
-#     experiment_title=experiment_title,
-#     runs_per_setting=runs_per_setting,
-#     variants=variants,
-#     log_dirs=log_dirs,
-#     common_args=(oc_key,),
-# )
+experiment_title = "A2OC_Pomdp"
+variant_levels = list()
+variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
+variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
+variant_levels.append(VariantLevel(delib_key, OC_DELIB, delib_names))  # Option deliberation cost
+variant_levels.append(VariantLevel(nopt_key, NUM_OPTIONS, nopt_names))  # Number of options
+variant_levels.append(VariantLevel(interest_key, INTEREST, int_names))  # Use of interest function
+variants, log_dirs = make_variants(*variant_levels)
+run_experiments(
+    script=path_a2oc,
+    affinity_code=affinity_code,
+    experiment_title=experiment_title,
+    runs_per_setting=runs_per_setting,
+    variants=variants,
+    log_dirs=log_dirs,
+    common_args=(oc_key,),
+)
 
 # A2OC RNN
 experiment_title = "A2OCRnn_Pomdp"
 variant_levels = list()
 variant_levels.append(VariantLevel(game_key, ENVS, env_names))  # pomdps
 variant_levels.append(VariantLevel(fomdp_key, FOMDP, obs_names))  # full or partial observability
+variant_levels.append(VariantLevel(delib_key, OC_DELIB, delib_names))  # Option deliberation cost
 variant_levels.append(VariantLevel(nopt_key, NUM_OPTIONS, nopt_names))  # Number of options
 variant_levels.append(VariantLevel(rnn_type_key, INTEREST, int_names))  # Use of interest function
 variant_levels.append(VariantLevel(rnn_type_key, RNN, rnn_names))  # Types of recurrency
--- rlpyt/experiments/scripts/pomdp/pg/train/pomdp_rnn_a2c_gpu.py
+++ rlpyt/experiments/scripts/pomdp/pg/train/pomdp_rnn_a2c_gpu.py
@@ -30,6 +30,7 @@ def build_and_train(slot_affinity_code, log_dir, run_ID, config_key):
     config = configs[config_key]
     variant = load_variant(log_dir)
     config = update_config(config, variant)
+    config["algo_name"] = 'A2C_RNN'
 
     sampler = GpuSampler(
         EnvCls=pomdp_interface,
